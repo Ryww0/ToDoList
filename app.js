6561eb2925ebd4185a5c7225b1e8205056bc2720
Ryww0 <laurent.chaitas@gmail.com>
2022-06-29 16:16:56 +0200

    function to remove tasks on clicking the trash button added

--- app.js
+++ app.js
@@ -7,16 +7,24 @@ function addTask() {
   if (inputAddTask.value === "") {
     return;
   } else {
-    newTask.innerHTML = `<div class="task flex m15"><p>${inputAddTask.value}</p></div>`;
+    newTask.innerHTML = `<div class="task flex m15 space-between"><p>${inputAddTask.value}</p><img class="trash" src="./images/trash_black.svg" alt="trash to delete"/></div>`;
     tasks.append(newTask);
     // after the new task is added, delete the input content
     inputAddTask.value = "";
+
+    //delete new task on click the trash (must be in the function addTask to work)
+    let btnDel = document.querySelectorAll("img");
+    btnDel.forEach((del) => {
+      del.addEventListener("click", () => {
+        del.parentElement.style.display = "none";
+      });
+    });
   }
 }
 
 // Add the task user's wrote when he click on the button
 btnAddTask.addEventListener("click", addTask);
-// Add the task user's wrote when he press enter on his keyboard
+// Add the task user's wrote when he press (not same as release the touch 'keyup') enter on his keyboard
 inputAddTask.addEventListener("keydown", function (e) {
   let newTask = document.createElement("div");
   // Only on enter key (keycode for enter is 13)
@@ -24,10 +32,17 @@ inputAddTask.addEventListener("keydown", function (e) {
     if (inputAddTask.value === "") {
       return;
     } else {
-      newTask.innerHTML = `<div class="task flex m15"><p>${inputAddTask.value}</p></div>`;
+      newTask.innerHTML = `<div class="task flex m15 space-between"><p>${inputAddTask.value}</p><img class="trash" src="./images/trash_black.svg" alt="trash to delete"/></div>`;
       tasks.append(newTask);
       // after the new task is added, delete the input content
       inputAddTask.value = "";
+
+      let btnDel = document.querySelectorAll("img");
+      btnDel.forEach((del) => {
+        del.addEventListener("click", () => {
+          del.parentElement.style.display = "none";
+        });
+      });
     }
   }
 });
