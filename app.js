21c7b1eb436e3d5d4a135b35d87436d45abc7f9e
Ryww0 <laurent.chaitas@gmail.com>
2022-06-29 14:34:36 +0200

    add the task on enter key pressed

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const btnAddTask = document.querySelector(".cross");
 const inputAddTask = document.querySelector(".input-task");
+const tasks = document.querySelector(".tasks");
 
 function addTask() {
-  const tasks = document.querySelector(".tasks");
   let newTask = document.createElement("div");
   if (inputAddTask.value === "") {
     return;
   } else {
     newTask.innerHTML = `<div class="task flex m15"><p>${inputAddTask.value}</p></div>`;
     tasks.append(newTask);
+    // after the new task is added, delete the input content
+    inputAddTask.value = "";
   }
 }
 
+// Add the task user's wrote when he click on the button
 btnAddTask.addEventListener("click", addTask);
+// Add the task user's wrote when he press enter on his keyboard
+inputAddTask.addEventListener("keydown", function (e) {
+  let newTask = document.createElement("div");
+  // Only on enter key (keycode for enter is 13)
+  if (e.keyCode === 13) {
+    if (inputAddTask.value === "") {
+      return;
+    } else {
+      newTask.innerHTML = `<div class="task flex m15"><p>${inputAddTask.value}</p></div>`;
+      tasks.append(newTask);
+      // after the new task is added, delete the input content
+      inputAddTask.value = "";
+    }
+  }
+});
