3addbd26c09af70e5229c1489a0bcbc309fdc614
Ryww0 <laurent.chaitas@gmail.com>
2022-06-29 13:57:18 +0200

    do not allow the user to add a task if the input is emty

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const inputAddTask = document.querySelector(".input-task");
 function addTask() {
   const tasks = document.querySelector(".tasks");
   let newTask = document.createElement("div");
-  newTask.innerHTML = `<div class="task flex m15"><p>${inputAddTask.value}</p></div>`;
-  tasks.append(newTask);
+  if (inputAddTask.value === "") {
+    return;
+  } else {
+    newTask.innerHTML = `<div class="task flex m15"><p>${inputAddTask.value}</p></div>`;
+    tasks.append(newTask);
+  }
 }
 
 btnAddTask.addEventListener("click", addTask);
